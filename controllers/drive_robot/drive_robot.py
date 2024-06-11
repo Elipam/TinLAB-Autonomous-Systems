704baf2e0261b4ba3e5ce9bc23ac1da2d308f036
XroosjeX <rosanneklok10@gmail.com>
2024-06-11 15:38:17 +0200

    Added functions for moving the robot on the grit.

--- controllers/drive_robot/drive_robot.py
+++ controllers/drive_robot/drive_robot.py
@@ -1,49 +1,126 @@
 from controller import Robot
-from controller import Motor
 
 # Create the robot instance
 robot = Robot()
 
-# Get the timestep of the current world
-timestep = 64
-max_speed = 6.28
+# Constants
+TIME_STEP = 64
+MAX_SPEED = 3
+WHEEL_RADIUS = 0.05
+LENGTH_SIDE = 1.0
 
-#Created motor instances
+# Calculate linear velocity and movement duration
+linear_velocity = WHEEL_RADIUS * MAX_SPEED
+duration_side = LENGTH_SIDE / linear_velocity
+
+# Get the motors
 leftfront_motor = robot.getDevice('Wheel1')
 leftback_motor = robot.getDevice('Wheel4')
 rightfront_motor = robot.getDevice('Wheel2')
 rightback_motor = robot.getDevice('Wheel3')
-leftfront_motor.setPosition(float('inf'))
-leftback_motor.setPosition(float('inf'))
-rightfront_motor.setPosition(float('inf'))
-rightback_motor.setPosition(float('inf'))
-leftfront_motor.setVelocity(0.0)
-leftback_motor.setVelocity(0.0)
-rightfront_motor.setVelocity(0.0)
-rightback_motor.setVelocity(0.0)
 
-# main loop
-while robot.step(timestep) != -1:
-# driving the robot in a straight line  
-    # left_speed = 0.5 * max_speed
-    # right_speed = 0.5 * max_speed
-    # leftfront_motor.setVelocity(left_speed)
-    # leftback_motor.setVelocity(left_speed)
-    # rightfront_motor.setVelocity(right_speed)
-    # rightback_motor.setVelocity(right_speed)
-# driving the robot the right on the same place
-    # left_speed = -0.5 * max_speed
-    # right_speed = 0.5 * max_speed
-    # leftfront_motor.setVelocity(left_speed)
-    # leftback_motor.setVelocity(left_speed)
-    # rightfront_motor.setVelocity(right_speed)
-    # rightback_motor.setVelocity(right_speed)
-# driving the robot to right with a bend
-    left_speed = 0.5 * max_speed
-    right_speed = 0.25 * max_speed
-    leftfront_motor.setVelocity(left_speed)
-    leftback_motor.setVelocity(left_speed)
-    rightfront_motor.setVelocity(right_speed) 
-    rightback_motor.setVelocity(right_speed)  
+# Initialize motors to infinity position and 0 velocity
+motors = [leftfront_motor, leftback_motor, rightfront_motor, rightback_motor]
+for motor in motors:
+    if motor is None:
+        raise ValueError("Error: One or more motors could not be found. Please check the device names.")
+    motor.setPosition(float('inf'))
+    motor.setVelocity(0.0)
+
+# Function to move a space forward
+def one_space():
+    start_time = robot.getTime()
+    end_time = start_time + 3.5  # Duration to drive forward
+    while robot.step(TIME_STEP) != -1:
+        current_time = robot.getTime()
+        if current_time < end_time:
+            left_speed = MAX_SPEED
+            right_speed = MAX_SPEED
+        else:
+            left_speed = 0
+            right_speed = 0
+        leftfront_motor.setVelocity(left_speed)
+        leftback_motor.setVelocity(left_speed)
+        rightfront_motor.setVelocity(right_speed)
+        rightback_motor.setVelocity(right_speed)
+        if current_time >= end_time:
+            break
+
+# function to move a space back
+def space_back():
+    start_time = robot.getTime()
+    end_time = start_time + 3.5  # Duration to drive forward
+    while robot.step(TIME_STEP) != -1:
+        current_time = robot.getTime()
+        if current_time < end_time:
+            left_speed = -MAX_SPEED
+            right_speed = -MAX_SPEED
+        else:
+            left_speed = 0
+            right_speed = 0
+        leftfront_motor.setVelocity(left_speed)
+        leftback_motor.setVelocity(left_speed)
+        rightfront_motor.setVelocity(right_speed)
+        rightback_motor.setVelocity(right_speed)
+        if current_time >= end_time:
+            break
+
+# Function to turn right by 45 degrees
+def turn_right():
+    start_time = robot.getTime()
+    end_time = start_time + (2 - 0.032) # Duration to turn 45 degrees (calibrate as needed)
+    print(start_time)
+    while robot.step(TIME_STEP) != -1:
+        current_time = robot.getTime()
+        if current_time < end_time:
+            left_speed = 0.5 * MAX_SPEED
+            right_speed = -0.5 * MAX_SPEED
+        else:
+            left_speed = 0
+            right_speed = 0
+        leftfront_motor.setVelocity(left_speed)
+        leftback_motor.setVelocity(left_speed)
+        rightfront_motor.setVelocity(right_speed)
+        rightback_motor.setVelocity(right_speed)
+        if current_time >= end_time:
+            break
+
+# Function to turn left by 45 degrees
+def turn_left():
+    start_time = robot.getTime()
+    end_time = start_time + (2 - 0.032)  # Duration to turn 45 degrees (calibrate as needed)
+    while robot.step(TIME_STEP) != -1:
+        current_time = robot.getTime()
+        if current_time < end_time:
+            left_speed = -0.5 * MAX_SPEED
+            right_speed = 0.5 * MAX_SPEED
+        else:
+            left_speed = 0
+            right_speed = 0
+        leftfront_motor.setVelocity(left_speed)
+        leftback_motor.setVelocity(left_speed)
+        rightfront_motor.setVelocity(right_speed)
+        rightback_motor.setVelocity(right_speed)
+        if current_time >= end_time:
+            break
+
+# function to move one space right
+def space_right():
+    turn_right()
+    one_space()
+    turn_left()
+
+# function to move one space left
+def space_left():
+    turn_left()
+    one_space()
+    turn_right()
+
+# Main loop
+while robot.step(TIME_STEP) != -1:
+    space_right()
+    break  # Exit after completing the sequence
+
+      
     
     
